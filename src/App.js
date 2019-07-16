3cb9c1337748d94c19c1e188e09086114290d7ad
Phani Rahul Sivalenka <rahul.sivalenka3@gmail.com>
2019-07-17 01:46:18 +0530

    (project setup) Add babel macros for better debugging of styled components

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React from 'react';
-import styled, { ThemeProvider } from 'styled-components';
+// Ref: https://www.styled-components.com/docs/tooling#babel-macro
+import styled, { ThemeProvider } from 'styled-components/macro';
 import { GlobalStyle } from './styles/GlobalStyle';
 import { theme } from './styles/Theme';
 
